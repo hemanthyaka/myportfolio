7adcc99c6612adaee79babdc507fb36b3f3ee0e1
hemanthyaka <hemanthyaka3@gmail.com>
2024-09-20 16:02:49 +0530

    second

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     <header>
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">YAKA HEMANTH</a>
+          <a className="navbar-brand" href="#home-section">YAKA HEMANTH</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
