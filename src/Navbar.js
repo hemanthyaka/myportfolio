889dae4994a25338df7e1bc83968fff6762e7758
hemanthyaka <hemanthyaka3@gmail.com>
2024-09-23 20:40:49 +0530

    first

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 import './NAvbar.css';
 
 const Navbar = () => {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
+  const closeNav = () => setIsNavCollapsed(true);
+
   return (
     <header>
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container-fluid">
           <a className="navbar-brand" href="#home-section">YAKA HEMANTH</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded={!isNavCollapsed ? true : false}
+            aria-label="Toggle navigation"
+            onClick={handleNavCollapse}
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarNav">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
                 <Link
@@ -22,6 +37,7 @@ const Navbar = () => {
                   offset={-70}
                   duration={100}
                   className="nav-link"
+                  onClick={closeNav}
                 >
                   Home
                 </Link>
@@ -35,6 +51,7 @@ const Navbar = () => {
                   offset={-70}
                   duration={100}
                   className="nav-link"
+                  onClick={closeNav}
                 >
                   About
                 </Link>
@@ -48,6 +65,7 @@ const Navbar = () => {
                   offset={-70}
                   duration={100}
                   className="nav-link"
+                  onClick={closeNav}
                 >
                   Skills
                 </Link>
@@ -61,6 +79,7 @@ const Navbar = () => {
                   offset={-70}
                   duration={100}
                   className="nav-link"
+                  onClick={closeNav}
                 >
                   Projects
                 </Link>
@@ -74,6 +93,7 @@ const Navbar = () => {
                   offset={-70}
                   duration={100}
                   className="nav-link"
+                  onClick={closeNav}
                 >
                   Contact
                 </Link>
